b3441c160b490e56876c50177f4b0fb3f5e17bc3
Tatiana Ignatova <tignatov@f0r4s19.codam.nl>
2025-08-14 14:46:46 +0200

    setting the work

---
+++ include/cub3d.h
@@ -0,0 +1,15 @@
+#ifndef CUB3D_H
+#define CUB3D_H
+
+
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "../MLX/include/MLX42/MLX42.h"
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+
+#endif
